cfd53d605f59749e8c9ee258d2520047636b8c5e
sabbirsr <rakibhasansabbir@yahoo.com>
2018-03-20 09:11:02 +0600

    seeding with faker part done successfully

--- database/factories/ModelStudentFactory.php
+++ database/factories/ModelStudentFactory.php
@@ -5,6 +5,6 @@ use Faker\Generator as Faker;
 $factory->define(App\Model\Student::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
-        'address' =>$faker->paragraph
+        'address' =>$faker->address
     ];
 });
